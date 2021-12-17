3d6a2d0493cad767bc6a19c9d1670208ac005f8f
Marcus Ho <marcus.ho@oocl.com>
2021-12-17 09:20:45 +0800

    [TodoBackEnd][marcus] feat: adavice

--- src/main/java/com/example/todoList/advice/ErrorResponse.java
+++ src/main/java/com/example/todoList/advice/ErrorResponse.java
@@ -1,4 +1,20 @@
 package com.example.todoList.advice;
 
 public class ErrorResponse {
+    private int code;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    private String message;
+
+    public ErrorResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
